4f71f395b6aa5dd79bea771df9be834b0d864324
Lia <liabifano@gmail.com>
2018-05-19 18:43:01 +0200

    statefulsets

--- run.sh
+++ run.sh
@@ -5,33 +5,43 @@ set -eo pipefail
 APP_NAME=hogwild
 REPO=liabifano
 KUBER_LOGIN=cs449g9
+N_WORKERS=5
 
-if ! [[ -z $(kubectl get pods | grep ${APP_NAME}) ]];
+# Don't forget to run login first with `docker login`
+docker login --username=$DOCKER_USER --password=$DOCKER_PASS
+
+# Delete services if exists
+if ! [[ -z $(kubectl get services | grep workers-service) ]];
 then
-    kubectl delete pods,services -l name=${APP_NAME}
-    echo "Shutting down last pod - ${APP_NAME} ..."
-    sleep 1m
+    kubectl delete -f Kubernetes/workers.yaml --cascade=true
+    echo "Shutting down workers"
 fi;
 
+if ! [[ -z $(kubectl get services | grep coordinator-service) ]];
+then
+    kubectl delete -f Kubernetes/coordinator.yaml --cascade=true
+    echo "Shutting down coordinator"
+fi;
 
 # build docker images
-# TODO: put a check if they already exists and to another to overwrite
-#cd Docker && docker-compose up
-docker build -f `pwd`/Docker/node/Dockerfile `pwd` -t ${REPO}/${APP_NAME}_node
-docker push ${REPO}/${APP_NAME}_node
+docker build -f `pwd`/Docker/Dockerfile `pwd` -t ${REPO}/${APP_NAME}
+docker push ${REPO}/${APP_NAME}
 
-docker build -f `pwd`/Docker/coordinator/Dockerfile `pwd` -t ${REPO}/${APP_NAME}_coordinator
-docker push ${REPO}/hogwild_coordinator
+# create workers service
+kubectl create -f Kubernetes/workers.yaml
+kubectl scale statefulset/worker --replicas=$N_WORKERS -n cs449g9
+# TODO: put a check until all the nodes are ready
+#kubectl create -f Kubernetes/coordinator.yaml # it will crash
 
-# TODO: Do it dynamically and also hog-pod.yaml
-# Create `pod` in the cluster
-kubectl create -f ./Kubernetes/hog-pod.yaml
-kubectl describe pod/${APP_NAME} -n ${KUBER_LOGIN}
+# useful commands
+#kubectl delete po,svc --all
+#docker build -f `pwd`/Docker/coordinator/Dockerfile `pwd` -t ${REPO}/${APP_NAME}_coordinator
+#docker push ${REPO}/hogwild_coordinator
+## Create `pod` in the cluster
+#kubectl create -f ./Kubernetes/hog-pod.yaml
+#kubectl describe pod/${APP_NAME} -n ${KUBER_LOGIN}
 # kubectl logs $APP_NAME -p --container="coordinator"
-
-#[QUESTION: I have to push all the images to a remote repository? I can do it from local images without use minikube?]
-#[QUESTION: Do we need a new image for each node? Or it is just change the port while it container is spin? How can we do it?]
-#[QUESTION: The data should be located in the volume provided and all cluster will have access to it, equals in the paper. Coordinator job is not distribute data anymore]
-
+#kubectl -n my-ns delete po,svc --all
+#kubectl delete -f Kubernetes/workers.yaml --cascade=true
 
 
